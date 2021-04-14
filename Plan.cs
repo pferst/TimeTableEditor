1844b6565188384f896dddde002bd3d677f75608
Piotr Ferst <piotr.ferst@o2.pl>
2021-04-14 21:03:39 +0200

    Gotowy projekt

---
+++ Plan.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+
+namespace TimetableEditor
+{
+    class Plan
+    {
+        //fields
+        private string fileContent, targetFile = "", defFName;
+        private bool notFounded = true, began = false;
+        private static bool isExecuted = false;
+
+        //properties
+        private string FileContent
+        {
+            get
+            {
+                return fileContent;
+            }
+            set
+            {
+                fileContent = value;
+            }
+        }
+        private string TargetFile
+        {
+            get
+            {
+                if (!String.IsNullOrWhiteSpace(targetFile))
+                {
+                    return targetFile;
+                }
+                return "empty data";
+            }
+            set
+            {
+                if (!String.IsNullOrWhiteSpace(value) && value != targetFile && value.EndsWith(".ics"))
+                {
+                    targetFile = value;
+                }
+            }
+        }
+        //constructors
+        public Plan(string fileName)
+        {
+            TargetFile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads\" + fileName + ".ics";
+            defFName = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads\" + fileName;
+        }
+
+        //methods
+        public static bool FileExists(string fileName)
+        {
+            return File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads\" + fileName + ".ics");
+        }
+        public void RemoveSubject(string subName)
+        {
+            if (File.Exists(this.TargetFile))
+            {
+                Console.WriteLine("File found");
+                StreamReader reader = new StreamReader(this.TargetFile);
+                string temp;
+                while (reader.Peek() != -1)
+                {
+                    temp = reader.ReadLine();
+                    if (!began)
+                    {
+                        if (temp.StartsWith("BEGIN:VEVENT"))
+                        {
+                            this.began = true;
+                        }
+                        else
+                        {
+                            SaveToNewFile(temp);
+                        }
+                    }
+                    if (temp.StartsWith("END:") && this.began)
+                    {
+                        if (this.notFounded)
+                        {
+                            SaveToNewFile(this.FileContent + temp);
+                        }
+                        this.FileContent = "";
+                        this.notFounded = true;
+                    }
+                    else if (this.began)
+                    {
+                        if (temp.ToUpper().Contains("SUMMARY:" + subName.Trim().ToUpper()))
+                        {
+                            this.notFounded = false;
+                        }
+                        else if (this.notFounded)
+                        {
+                            this.FileContent += temp + "\n";
+                        }
+                    }
+                }
+                reader.Close();
+                this.began = false;
+                MoveFinalFile();
+                Console.WriteLine("Finished");
+            }
+            else
+            {
+                Console.WriteLine("File didn't find - {0}", this.TargetFile);
+            }
+        }
+        private void SaveToNewFile(string content)
+        {
+            string destFile = "temp.ics";
+            StreamWriter writer;
+            if (!isExecuted)
+            {
+                isExecuted = true;
+                writer = new StreamWriter(destFile);
+            }
+            else
+            {
+                writer = new StreamWriter(destFile, true);
+            }
+            writer.WriteLine(content);
+            writer.Flush();
+            writer.Close();
+        }
+        private void MoveFinalFile()
+        {
+            this.TargetFile = this.defFName + "_Edited.ics";
+            File.Move("temp.ics", this.TargetFile, true);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Threading;
+
+
+namespace TimetableEditor
+{
+    class Program
+    {
+        static void Main()
+        {
+            WelcomeMessage();
+            Console.WriteLine("Is your file is in Downloads? Y/N");
+            if (Console.ReadKey().KeyChar.ToString().ToLower() == "n")
+            {
+                Console.WriteLine("\nPlease move it there, others options in future realeses");
+                Thread.Sleep(5000);
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.Write("\nFile name with you timetable (.ics): ");
+                string fileName = Console.ReadLine();
+                while (!Plan.FileExists(fileName))
+                {
+                    Console.WriteLine("Sorry, but tht file not exists :( - check downloads directory");
+                    Console.Write("\nFile name with you timetable (.ics): ");
+                    fileName = Console.ReadLine();
+                }
+                Plan newTimetable = new Plan(fileName); ;
+                string theEnd;
+                do
+                {
+                    Console.Write("Short name of subject to delete: ");
+                    string sn = Console.ReadLine();
+                    Console.Write("TYPE of lessons from given subejct to delete: ");
+                    string tos = Console.ReadLine();
+                    newTimetable.RemoveSubject(sn + " " + tos);
+                    JustStars();
+                    Console.WriteLine("Do you want to delete more subjects? Y/N");
+                    theEnd = Console.ReadKey().KeyChar.ToString().ToLower();
+                    Console.WriteLine();
+
+                } while (theEnd == "y");
+                newTimetable = null;
+            }
+        }
+        private static void WelcomeMessage()
+        {
+            string[] rules = new string[]
+            {
+                "Default directory for program is Downloads on you computer",
+                "You have to write correct name of file without file extension - otherwise it won't work",
+                "Write correct short name of subject you want to delete",
+                "2 separate instructions will appear for subject name and type",
+                "If you want remove all type of lessons from given subject use [Enter]",
+                "Specific type from given subject - just write name like \"lab\" \"ćw\" etc."
+            };
+            JustStars();
+            Console.WriteLine("\tWelcome user");
+            Console.WriteLine("\nInstructions:");
+            for (int i = 0; i < rules.Length; i++)
+            {
+                Console.WriteLine("{0}. {1}", i + 1, rules[i]);
+            }
+            JustStars();
+            Console.Write("\n");
+        }
+        private static void JustStars()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
